64b3b0bfcc5ddb3a379ebd94ad7e43129ce3fb5b
dayyalasasikala <154111606+dayyalasasikala@users.noreply.github.com>
2023-12-24 09:46:36 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,26 @@
+
+public class Array {
+	public static void main(String[] args) {
+		int i[];
+		int[]j=new int[9];
+		
+		
+		
+		j=new int[] {45,35,78,67,19};
+		j[0]=4;
+		j[1]=6;
+		j[7]=4;
+		j[5]=8;
+		
+		
+		
+		
+
+// int sum=j[0]+j[1]+j[7]+j[5];
+//   System.out.println(sum);
+	
+	}
+		
+		
+	}
+
