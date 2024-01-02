6473db4ef43eef0c4ce936bdc88d847517534615
dayyalasasikala <154111606+dayyalasasikala@users.noreply.github.com>
2024-01-02 11:03:56 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,19 @@
+package org.jsp.numdemo;
+
+import java.util.Scanner;
+
+public class Mul {
+
+	public static void main(String[] args) {
+		int r,s;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first number");
+		r=sc.nextInt();
+		System.out.println("Enter second number");
+		s=sc.nextInt();
+		 int q=r*s;
+		 System.out.println("mul of numbers is: "+q);
+		
+	}
+
+}
