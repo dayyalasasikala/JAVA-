92ac979756fdc4bdd22d407543616cdaadaaa8b9
dayyalasasikala <154111606+dayyalasasikala@users.noreply.github.com>
2024-01-03 22:27:26 +0530

    Add files via upload

---
+++ tester.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class tester
+{
+public static void main(java.lang.String[] args) 
+{
+   Scanner sc=new Scanner(System.in);
+				System.out.println("enter the sentence");
+				String Str=sc.findInLine(56);
+				String[] words=Str.split(" ");
+				System.out.println(words.length);
+				
+			}
+
+		}
+
