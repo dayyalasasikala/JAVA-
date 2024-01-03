8a5fa8ca62a90eaa5227407e93f98e994d0e0ef5
dayyalasasikala <154111606+dayyalasasikala@users.noreply.github.com>
2024-01-03 21:44:08 +0530

    Add files via upload

---
+++ Chocolates.java
@@ -0,0 +1,17 @@
+
+public class Chocolates {
+
+	public static void main(String[] args) {
+     int n1=3;
+     int n2=9;
+     int n3=5;
+     int res1=n1+++n3++;
+     int res2=++n2-n1+n2;
+     int res3=n3+++n2++ + ++n1;
+     System.out.println(res1);
+     System.out.println(res2);
+     System.out.println(res3);
+     
+	}
+
+}
