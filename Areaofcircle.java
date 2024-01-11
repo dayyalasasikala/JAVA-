eb720949ee93d03f6fa4dd0f3c25b9588df618f6
D Sasikala <154111606+dayyalasasikala@users.noreply.github.com>
2024-01-11 21:32:56 +0530

    Add files via upload

---
+++ Areaofcircle.java
@@ -0,0 +1,16 @@
+package org.com.praveendemo;
+
+public class Areaofcircle {
+
+	public static void main(String[] args) {
+     
+		double pi=3.14,r=2.85,area;
+		area=pi*r*r;
+		System.out.println("area of circle is");
+		System.out.println(area);
+		
+		
+	}
+
+}
+//formula is pi*r2     
