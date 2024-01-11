d24abe6bf4b2e58340e7ee220a6a7b6a7f9cf56a
D Sasikala <154111606+dayyalasasikala@users.noreply.github.com>
2024-01-11 22:03:06 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,17 @@
+package org.com.praveendemo;
+
+import java.util.Scanner;
+
+public class Circle {
+
+	public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     float r=sc.nextFloat();
+     float pi=3.141f;
+     float perimeter=2*pi*r;
+     System.out.println("perimeter of circle is ..");
+     
+     
+	}
+
+}
