d7bd9beae27c28f36b3d04b8b51f0af96ee1c665
D Sasikala <154111606+dayyalasasikala@users.noreply.github.com>
2024-01-11 21:16:20 +0530

    Add files via upload

---
+++ Anynum.java
@@ -0,0 +1,13 @@
+package org.com.praveendemo;
+
+public class Anynum {
+
+	public static void main(String[] args) {
+		
+    
+     for(int i=1;i<=10;i++) {
+    	 System.out.println("4 *" +i+ " = " +4 * i);
+     }
+	}
+
+}
