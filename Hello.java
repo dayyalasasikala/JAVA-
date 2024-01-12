9543bdbc4773520fe28e422f7a64b0320600cafc
D Sasikala <154111606+dayyalasasikala@users.noreply.github.com>
2024-01-12 21:01:26 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package org.com.praveendemo;
+
+public class Hello {
+	public static void main(String[] args) {
+		short s=2;
+	    int i=s;
+	    byte b=4;
+	    double d=b;
+	    System.out.println(s);
+	    System.out.println(i);
+	    System.out.println(b);
+	    System.out.println(d);
+	}
+
+}
+
+
+			
