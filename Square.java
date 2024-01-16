3887d274c045371fb3c4c0652792dda41fbcb523
D Sasikala <154111606+dayyalasasikala@users.noreply.github.com>
2024-01-16 15:07:53 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,14 @@
+package org.com.praveendemo;
+
+public class Square {
+
+public static void main(String[] args) {
+   int side =4;
+   int perimeter=4*side;
+     
+    	System.out.println("perimeter of square is:"+perimeter);
+     
+	}
+
+}
+// formula is side*side
