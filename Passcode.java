f21e22d5b9ebad2e7c68fbd378c65ac42ba3dfee
D Sasikala <154111606+dayyalasasikala@users.noreply.github.com>
2024-02-18 20:57:52 +0530

    Add files via upload

---
+++ Passcode.java
@@ -0,0 +1,23 @@
+package org.jsp.sasidemo;
+
+public class Passcode {
+
+	public static void main(String[] args) {
+    int n=4;
+    for(int i=1;i<=n;i++)
+    {
+    	for(int j=1;j<n-i;j++) 
+    	{
+    		System.out.print(" ");
+    	}
+    	for(int k=1;k<=i;k++) {
+    		System.out.print(k);
+    	}
+    	for(int j=i-1;j>=1;j--) {
+    		System.out.print(j);
+    	}
+    	System.out.println();
+    }
+	}
+
+}
