0d90407aa37b1553b4f680b17571c9719c7398f5
D Sasikala <154111606+dayyalasasikala@users.noreply.github.com>
2024-06-30 19:25:22 +0530

    Add files via upload

---
+++ Widening.java
@@ -0,0 +1,18 @@
+
+public class Widening{
+	 public static void main(String[]args) {
+		 int n=8;
+		 float f=n;
+		 System.out.println("n");
+		 System.out.println("f");
+		  
+		 short s=7;
+		 double d=s;
+		 System.out.println("s");
+		 System.out.println("d");
+		 
+		 
+		 
+	}
+
+}
